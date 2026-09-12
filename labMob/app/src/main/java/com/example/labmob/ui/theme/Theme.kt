bfc2cafb616239116d1dc8e3254a89a4c5fa16c8
Пырцак Герман FO1ULI <rbebi96@mail.ru>
2026-09-12 23:06:56 +0700

    style: оформить визуальную тему и добавить графические ресурсы

--- labMob/app/src/main/java/com/example/labmob/ui/theme/Theme.kt
+++ labMob/app/src/main/java/com/example/labmob/ui/theme/Theme.kt
@@ -1,58 +1,31 @@
 package com.example.labmob.ui.theme
 
-import android.app.Activity
-import android.os.Build
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.graphics.Color
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
-)
-
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
+private val ComicColorScheme = darkColorScheme(
+    primary = Color(0xFFE60012),
     onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    secondary = Color.White,
+    onSecondary = Color(0xFF07070A),
+    background = Color(0xFF07070A),
+    onBackground = Color.White,
+    surface = Color(0xFF17171D),
+    onSurface = Color.White,
+    error = Color(0xFFFF4051),
 )
 
 @Composable
 fun LabMobTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
-    content: @Composable () -> Unit
+    darkTheme: Boolean = false,
+    dynamicColor: Boolean = false,
+    content: @Composable () -> Unit,
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
-    }
-
     MaterialTheme(
-        colorScheme = colorScheme,
+        colorScheme = ComicColorScheme,
         typography = Typography,
-        content = content
+        content = content,
     )
 }
